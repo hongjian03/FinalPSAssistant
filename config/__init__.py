dfb3247043f06ef00929df852c9f379fef69d2ab
LittleBai25 <stupidcat0@163.com>
2025-05-19 10:35:35 +0800

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,2 +1,7 @@
 # Config package initialization
 # This file makes the config directory a proper Python package 
+
+from . import prompts
+from . import jina_config
+
+__all__ = ['prompts', 'jina_config'] 
