6f547b286fa7af4a19d232ae57b010d9451a0941
LittleBai25 <stupidcat0@163.com>
2025-05-19 16:38:32 +0800

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -8,7 +8,6 @@ from .transcript_analyzer import TranscriptAnalyzer
 from .serper_client import SerperClient
 
 # 导入PS Assistant新Agent
-from .ps_info_collector import PSInfoCollector
 from .supporting_file_analyzer import SupportingFileAnalyzer
 from .ps_analyzer import PSAnalyzer
 from .ps_rewriter import PSRewriter
